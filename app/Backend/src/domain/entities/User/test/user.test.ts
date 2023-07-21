2a88e913a34df09aa53ba09db596f111830a125d
Rodrigo <rodrigoga701@gmail.com>
2023-07-20 23:44:23 -0300

    test: TDD - domain user creation

---
+++ app/Backend/src/domain/entities/User/test/user.test.ts
@@ -0,0 +1,20 @@
+import { it, describe, expect, beforeEach } from 'vitest';
+import { User } from '../User';
+import { EMAIL, PASSWORD, USERNAME, CLASS_PROPERTIES} from './mocks/user.mock';
+
+describe('User Domain', () => {
+  describe('in case of success', () => {
+    let user: User;
+    beforeEach(async () => {
+      user = new User(EMAIL, PASSWORD, USERNAME);
+    });
+    it('should be possible to create a User instance', () => {
+      expect(user).toBeInstanceOf(User);
+    });
+    it('there must be all the correct properties in the User class', () => {
+      CLASS_PROPERTIES.forEach((pro) => {
+        expect(user).toHaveProperty(pro); 
+      });
+    });
+  });
+});
