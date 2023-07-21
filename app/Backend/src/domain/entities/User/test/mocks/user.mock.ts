4a57c7e5363011f8936871616ae0ee73260372a8
Rodrigo <rodrigoga701@gmail.com>
2023-07-21 18:00:17 -0300

    test: TDD - domain user creation

---
+++ app/Backend/src/domain/entities/User/test/mocks/user.mock.ts
@@ -0,0 +1,4 @@
+export const EMAIL = 'bruceWane@gmail.com';
+export const PASSWORD = 'bat-password';
+export const USERNAME = 'Bruce Wane';
+export const CLASS_PROPERTIES = ['email', 'password', 'userName', 'id', 'image'];
