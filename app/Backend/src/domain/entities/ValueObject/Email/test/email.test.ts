0e806f59513f497c37237b98561bacd0b605bdad
Rodrigo <rodrigoga701@gmail.com>
2023-07-22 00:13:12 -0300

    test: TDD - value object Email creation

---
+++ app/Backend/src/domain/entities/ValueObject/Email/test/email.test.ts
@@ -0,0 +1,17 @@
+import {it, describe, expect} from 'vitest';
+import {Email} from '../Email';
+import {EMAIL_INVALID, EMAIL_VALID} from './mocks/email.mock';
+
+describe('Email Value Object', () => {
+  describe('in case of success', () => {
+    it('must return the email if it is in valid format', () => {
+      const email = new Email(EMAIL_VALID);
+      expect(email.value).to.equal(EMAIL_VALID);
+    });
+    describe('in case of error', () => {
+      it('must throw an error if email is in invalid format', () => {
+        expect(() => new Email(EMAIL_INVALID)).toThrowError('"email" must be a valid email');
+      });
+    });
+  });
+});
---
+++ app/Backend/src/domain/entities/ValueObject/Email/test/mocks/email.mock.ts
@@ -0,0 +1,2 @@
+export const EMAIL_VALID = 'bruceWane@gmail.com';
+export const EMAIL_INVALID = 'batman@batman@batman@batman.com';
