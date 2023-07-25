4c2653b0a9569a3a8eb21847ef49c0c18f6e8d60
Rodrigo <rodrigoga701@gmail.com>
2023-07-25 11:05:11 -0300

    test: TDD - domain Categories creation

---
+++ app/Backend/src/domain/entities/Categories/test/categories.test.ts
@@ -0,0 +1,20 @@
+import { it, describe, expect, beforeEach } from 'vitest';
+import { Categories } from '../Categories';
+import { CATEGORY, CLASS_PROPERTIES } from './mocks/categories.mock';
+
+describe('Categories Domain', () => {
+  describe('in case of sucess', () => {
+    let categorie: Categories;
+    beforeEach(() => {
+      categorie = new Categories(CATEGORY);
+    } );
+    it ('should be possible to create a Categories instance', () => {
+      expect(categorie).toBeInstanceOf(Categories);
+    });
+    it('there must be all the correct proerties in the Blog Posts class', () => {
+      CLASS_PROPERTIES.forEach((pro) => {
+        expect(categorie).toHaveProperty(pro);
+      });
+    });
+  });
+});
---
+++ app/Backend/src/domain/entities/Categories/test/mocks/categories.mock.ts
@@ -0,0 +1,2 @@
+export const CATEGORY = 'Super Hero';
+export const CLASS_PROPERTIES = ['id', 'categoryName'];
