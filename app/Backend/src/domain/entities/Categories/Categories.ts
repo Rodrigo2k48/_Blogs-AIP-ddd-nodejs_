8ef53f13e4e7543dec05469193f8efd03484dc29
Rodrigo <rodrigoga701@gmail.com>
2023-07-25 11:17:34 -0300

    feat: Categories domain

---
+++ app/Backend/src/domain/entities/Categories/Categories.ts
@@ -0,0 +1,21 @@
+interface CategoriesInterface {
+  id? : number;
+  categoryName: string;
+}
+
+export class Categories implements CategoriesInterface {
+  private readonly _id?: number; 
+  private _categoryName: string;
+
+  constructor(categoryName: string) {
+    this._categoryName = categoryName;
+  }
+
+  get id(): number | undefined { 
+    return this._id;
+  } 
+  get categoryName(): string {
+    return this._categoryName;
+  }
+
+}
