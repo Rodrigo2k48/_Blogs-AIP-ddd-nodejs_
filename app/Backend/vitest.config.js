d7cfd2221303feeb6b240b38975026e31f3f2cd0
Rodrigo Gonçalves <rodrigoga701@gmail.com>
2023-12-06 15:45:00 -0300

    Revert "chore: ceate vitest.config for coverage configuration, BREAKING CHANGE: changing the version of vitest and vitest/coverage-v8 for bug reasons, now vitest are stuck at version 1.0.1 in the package (I found another better solution)"
    This reverts commit b45de06275fcfc0f8aa546155fa232d4037caf53.

--- app/Backend/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    coverage: {
-      exclude: ['**/src/infrastructure/orm/**', '**/src/application/webService/**']
-    },
-  },
-});
